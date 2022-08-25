cdc1793d60902b4a9f5266185c0ddde540c9a004
ilerena <ignaciolerena@hotmail.com>
2022-08-25 17:13:40 -0300

    js terminado

--- js.js
+++ js.js
@@ -11,19 +11,44 @@ class Jugadores{
 }
 
 let plantel = [];
+let documentos = [];
 
-let nuevo_registro = new Jugadores("ignacio", "lerena", 38054175, 4, 187, 115);
-plantel.push(nuevo_registro);
+let jugador1 = new Jugadores("ignacio", "lerena", 38054175, 4, 187, 115);
+plantel.push(jugador1);
+jugador2 = new Jugadores("Lucas", "Clarens", 35441839, 2, 180, 100);
+plantel.push(jugador2);
+jugador3 = new Jugadores("Joaco", "Moine", 40111222, 5, 190, 110);
+plantel.push(jugador3);
 
-console.log(plantel);
-
+for (let docs of plantel){
+    documentos.push(docs.dni);
+}
 
 let bienvenido = parseInt(prompt("Bienvenido, ingrese su dni"));
 
+function monitoreo (){
+    let ultimo_partido = prompt("¿Jugaste el sabado? si o no");
+    if (ultimo_partido == "si"){
+    let minutos_ultimo_partido = parseInt(prompt("¿Cuantos Minutos jugaste el ultimo partido?"));
+        if (minutos_ultimo_partido < 50){
+            console.log("Puede entrenar normalmente!")
+        } else{
+            console.log("Debes hacer recuperacion en hielo antes de la practica del lunes");
+        }
+    } else {
+    let lesion = prompt("¿Estuviste lesionado?");
+        if (lesion == "si"){
+        console.log("Continua con tu recuperación!");
+        }  else {
+        console.log("Te esperamos en practica!");
+        }
+    }
+}
 
 
-if (plantel.includes(bienvenido)){
-    console.log("Bienvenido", plantel.nombre);
+if (documentos.includes(bienvenido)){
+    console.log("Bienvenido");
+    monitoreo();
 } else {
     console.log("Debe registrarse como nuevo jugador");
     let nombre = prompt("Ingrese su nombre");
@@ -36,21 +61,7 @@ if (plantel.includes(bienvenido)){
     let nuevo_registro = new Jugadores(nombre, apellido, dni, puesto, altura, peso);
     plantel.push(nuevo_registro);
 
+    console.log(`Bienvenido ${nombre}`);
+    monitoreo();
+
 }
-
-console.log(plantel);
-
-let ultimo_partido = prompt("¿Jugaste el sabado? si o no");
-    if (ultimo_partido == "si"){
-    let minutos_ultimo_partido = parseInt(prompt("¿Cuantos Minutos jugaste el ultimo partido?"));
-} else {
-    let lesion = prompt("¿Estuviste lesionado?");
-    let proximo_partido = prompt("¿Estas en condiciones de jugar el siguiente partido?");
-    if (proximo_partido == "si"){
-        console.log("Te esperamos en practica!");
-    } else {
-        console.log("Continua con tu recuperación!");
-    }
-}
-
-console.log(plantel);
